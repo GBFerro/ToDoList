d6d4d5ece6c5a3ac5dc8d10d3e40a74b29f89419
GBFerro <giovani.ferro@usp.br>
2023-03-26 21:57:04 -0300

    Criado menu de interação com o usuario

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,5 +1,87 @@
-﻿using TodoList;
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        do
+        {
+            Console.Clear();
 
-ToDo todo = new ToDo();
+            int op = Menu();
+            switch (op)
+            {
+                case 1:
 
-Console.WriteLine(todo.Id);
+                    Console.WriteLine("Aperte qualquer tecla para continuar");
+                    Console.ReadKey();
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Aperte qualquer tecla para continuar");
+                    Console.ReadKey();
+                    break;
+
+                case 3:
+
+                    Console.WriteLine("Aperte qualquer tecla para continuar");
+                    Console.ReadKey();
+                    break;
+
+                case 4:
+
+                    Console.WriteLine("Aperte qualquer tecla para continuar");
+                    Console.ReadKey();
+                    break;
+
+                case 5:
+
+                    Console.WriteLine("Aperte qualquer tecla para continuar");
+                    Console.ReadKey();
+                    break;
+
+                case 6:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida");
+                    Console.WriteLine("Aperte qualquer tecla para continuar");
+                    Console.ReadKey();
+                    break;
+            }
+
+        } while (true);
+
+
+    }
+
+    private static int Menu()
+    {
+        Console.Clear();
+        Console.WriteLine(">>>Menu de opções<<<\n\n1 - Criar Tarefa \n2 - Cadastrar Pessoas\n" +
+            "3 - Criar Categorias\n4 - Concluir Tarefa\n5 - Listar Tarefas\n6 - Sair\n\n" +
+            "Escolha uma opção: ");
+
+        var aux = IsInt();
+
+        return aux;
+
+    }
+
+    private static int IsInt()
+    {
+        int value;
+        do
+        {
+            if (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("Digite um número inteiro");
+            }
+            else
+            {
+                return value;
+            }
+
+        } while (true);
+    }
+}
